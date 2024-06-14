b51b9032411930f59e67b78357c56edcfdb19a4a
aakashsaini09 <aakashsaini948585@gmail.com>
2024-06-14 16:57:30 +0530

    insertUser Api done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("working")
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
+
+async function insertUser(username: string, password: string, firstname: string, lastname: string){
+    const response = await prisma.user.create({
+
+        data:{
+            username, 
+            password, 
+            firstname, 
+            lastname, 
+        }
+    })
+    console.log(response)
+}
+insertUser("akash2", "abcd2", "aakash2", "saini2")

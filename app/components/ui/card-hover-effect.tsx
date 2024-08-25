54498947ca2d52784738dbfad02fc9d2decad243
aakashsaini09 <aakashsaini948585@gmail.com>
2024-08-25 15:27:40 +0530

    img fix

--- app/components/ui/card-hover-effect.tsx
+++ app/components/ui/card-hover-effect.tsx
@@ -7,15 +7,15 @@ import logo from '../../assets/frontend.png'
 import { useState } from "react";
 
 export const HoverEffect = ({items,className,}: {
-  items: {title: string;description: string;link: string;}[];
+  items: {title: string;description: string;link: string; img: string}[];
   className?: string;
 }) => {
   let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
   return (
     <div
-      className={cn("grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 bg-slate-950 py-44",className)}>
+      className={cn("grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-slate-950 py-44",className)}>
       {items.map((item, idx) => (
-        <Link href={item?.link} key={item?.link} className="relative group block p-2 h-full w-full"onMouseEnter={() => setHoveredIndex(idx)}onMouseLeave={() => setHoveredIndex(null)}>
+        <Link href={item?.link} key={item?.link} className="relative group flex flex-wrap p-2 h-full w-full"onMouseEnter={() => setHoveredIndex(idx)}onMouseLeave={() => setHoveredIndex(null)}>
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span className="absolute inset-0 h-full w-full bg-purple-700 dark:bg-slate-800/[0.8] block shadow-md shadow-purple-600 rounded-3xl" layoutId="hoverBackground" initial={{ opacity: 0 }}
@@ -23,6 +23,17 @@ export const HoverEffect = ({items,className,}: {
             )}
           </AnimatePresence>
           <Card>
+            <div className="img w-full h-60 overflow-hidden">
+              <Image
+                    src={item?.img}
+                    alt={item?.img}
+                    className={"object-cover object-center w-full h-full"}
+                    width={0}
+                    height={0}
+                    loading="lazy"
+                    sizes="100vw"
+                  />
+            </div>
             <CardTitle>{item.title}</CardTitle>
             <CardDescription>{item.description}</CardDescription>
           </Card>
@@ -45,12 +56,12 @@ export const Card = ({
   return (
     <div
       className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-purple-700 dark:border-white/[0.2] group-hover:border-slate-700 relative z-10",
+        "rounded-2xl h-[55vh] w-full p-0 overflow-hidden bg-black border border-purple-700 dark:border-white/[0.2] group-hover:border-slate-700 relative z-10",
         className
       )}
     >
       <div className="relative z-50">
-        <div className="p-4">{children}</div>
+        <div className="p-0">{children}</div>
       </div>
     </div>
   );
@@ -63,9 +74,9 @@ export const CardTitle = ({
   children: React.ReactNode;
 }) => {
   return (
-    <Image src={logo} alt="not found" className={cn("text-zinc-100 font-bold tracking-wide mt-4 mx-auto flex justify-center text-center", className)}>
-      {/* {children} */}
-    </Image>
+      <div className="text-zinc-100 font-bold tracking-wide mt-4 text-2xl mx-auto flex justify-center text-center">
+        {children}
+      </div>
   );
 };
 export const CardDescription = ({
--- app/page.tsx
+++ app/page.tsx
@@ -3,38 +3,63 @@ import Hero from "./_comp/Hero";
 import TimeLine from "./_comp/TimeLine";
 import { HoverEffect } from "./components/ui/card-hover-effect";
 import image from "./assets/frontend.png"
-import Image from "next/image";
+import Image from "./image.svg";
 export default function Home() {
+  const linkof: string = '/assets/frontend.png'
   const content= [
     {
-      title: "Node-JS",
-      description: "",
-      link: "https://www.google.com"
+      title: "Tools-Hub",
+      description: "Get access to many useful tools at one place for free.",
+      link: "https://www.google.com/fkds",
+      img: '/frontend.png'
     },
     {
-      title: "React-JS",
-      description: "",
-      link: "https://www.google.com"
+      title: "QuillVibes",
+      description: "Users can write and publish their own blogs or generate content with AI assistance. The scalable, open- source web app includes multiple features.",
+      link: "https://www.google/gj.com",
+      img: '/github.png'
     },
     {
       title: "MongoDB",
-      description: "",
-      link: "https://www.google.com"
+      description: "this is and op website",
+      link: "https://www.google/rssg.com",
+      img: '/coin.png'
     },
     {
       title: "study",
       description: "",
-      link: "https://www.google.com"
+      link: "https://www.google/rws.com",
+      img: '/frontend.png'
     },
     {
       title: "study",
       description: "",
-      link: "https://www.google.com"
+      link: "https://www.google/fef.com",
+      img: '/frontend.png'
     },
     {
       title: "study",
       description: "",
-      link: "https://www.google.com"
+      link: "https://www.google/fds.com",
+      img: '/frontend.png'
+    },
+    {
+      title: "study",
+      description: "",
+      link: "https://www.google/rew.com",
+      img: '/frontend.png'
+    },
+    {
+      title: "study",
+      description: "",
+      link: "https://www.google/news/.com",
+      img: '/frontend.png'
+    },
+    {
+      title: "study",
+      description: "",
+      link: "https://www.google/images.com",
+      img: '/frontend.png'
     },
     
     
@@ -43,7 +68,7 @@ export default function Home() {
     <main className="h-[200vh] w-full bg-black">
       <Hero/>
       <TimeLine/>
-      <div className="bg-gray-950 px-20 gap-6">
+      <div className=" px-28 gap-6 min-h-[50vh] bg-gray-950">
         <HoverEffect items={content}/>
       </div>
 

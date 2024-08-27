c1137b22e21e80e20e3a5baa6e28c67e053b6f50
aakashsaini09 <aakashsaini948585@gmail.com>
2024-08-27 15:40:08 +0530

    init

--- app/_comp/Hero.tsx
+++ app/_comp/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import '../assets/style.css'
 import { FlipWords } from "../components/ui/flip-words"
 const Hero = () => {
   const words = ["developer", "designer"]
@@ -8,12 +9,10 @@ const Hero = () => {
         <div className="px-24 font-extralight text-8xl text-white">Hi! I'm</div>
         <div className="px-24 font-bold text-white text-8xl font-mono">Aakash Saini</div>
         <div className="text-white font-bold text-4xl px-24">I'm a web <FlipWords words={words} className="text-purple-600"/></div>
-        <hr className="mx-24 h-0 border border-black w-72"/>
-        <p className="font-medium text-slate-500 text-base px-24">I'm a pasonite Full Stack developer with a good knowledge of web3. I'm a pasonite Full Stack developer with a good knowledge of web3. I'm a pasonite Full Stack developer with a good knowledge of web3.</p>
+        <p className="font-medium text-slate-500 text-base px-24 pt-3">I'm a pasonite Full Stack developer with a good knowledge of web3. I'm a pasonite Full Stack developer with a good knowledge of web3. I'm a pasonite Full Stack developer with a good knowledge of web3.</p>
       </div>
 
-      <div className="right w-[40%] h-[90vh]">
-      </div>
+      <div className="right w-[40%] h-[90vh] flex justify-center items-center"></div>
     </div>
   )
 }
